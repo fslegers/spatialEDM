ce461c11afa143edd2ead4c89a4b4c10ded1f135
Fleur Slegers <f.slegers@uu.nl>
2023-05-31 15:12:37 +0200

    end of workday

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,13 @@
 import numpy as np
+import sklearn.preprocessing
 from matplotlib import pyplot as plt
 
 
 def summarize_statistics(time_series, window_size = 10, filename = ""):
+    """
+    Plots a single time series, its moving average and its moving variance.
+    Can be used to detect nonstationarity.
+    """
     time_series = time_series.tolist()
 
     means = []
@@ -32,9 +37,29 @@ def summarize_statistics(time_series, window_size = 10, filename = ""):
 
     plt.show()
 
+def normalize_time_series(time_series):
+    """
+    Standardizes each series in time_series.
+    :param time_series: ndarray of a single or multiple time series.
+    :return standardized time series with mean 0 and unit variance
+    """
+
+    scaler = sklearn.preprocessing.StandardScaler().fit(time_series)
+    time_series_scaled = scaler.transform(time_series)
+
+    print(time_series_scaled.mean(axis = 0))
+    print(time_series_scaled.std(axis = 0))
+
+    return(time_series_scaled)
+
+
 if __name__ == "__main__":
     x = np.arange(0,10,0.1)
     y = np.sin(x)
 
     summarize_statistics(x, window_size=20)
     summarize_statistics(y, window_size=20)
+
+    x_normalized = normalize_time_series(np.array([x]).T)
+
+    xy_normalized = normalize_time_series(np.array([x,y]))
