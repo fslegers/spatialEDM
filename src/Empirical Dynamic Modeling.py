aca1a37a12b9ed51cd0ed77124ab6844e9717853
Fleur Slegers <f.slegers@uu.nl>
2023-06-12 16:22:07 +0200

    Implemented S-Map regression

--- src/Empirical Dynamic Modeling.py
+++ src/Empirical Dynamic Modeling.py
@@ -22,7 +22,8 @@ def simplex_projection(time_series, lag = -1, max_E = 10):
     test_set = str(floor(0.6 * length) + 1) + " " + str(length)
 
     # Plot and return prediction skill rho for each embedding via Simplex
-    rho_per_E = EmbedDimension(dataFrame = df, maxE = max_E, tau = -np.abs(lag), lib = training_set, pred = test_set, columns = "x")
+    rho_per_E = EmbedDimension(dataFrame = df, maxE = max_E, tau = -np.abs(lag),
+                               lib = training_set, pred = test_set, columns = "x")
 
     # Find the optimal E
     optimal_param = rho_per_E.loc[rho_per_E['rho'].idxmax()]
@@ -31,19 +32,49 @@ def simplex_projection(time_series, lag = -1, max_E = 10):
 
     return(optimal_param["E"])
 
+def S_map(time_series, lag = -1, E = 10):
+    """
+    Evaluates Smap prediction skill for different values of localization parameter theta)
+    :param time_series:
+    :param lag:
+    :param E:
+    :return:
+    """
+    # If no observation times are given, add them to time_series
+    if len(np.shape(time_series)) == 1:
+        obs_times = np.arange(1, np.shape(time_series)[0] + 1, 1)
+        time_series = np.column_stack((obs_times, time_series))
+
+    # Turn time_series into pandas dataframe (column vector)
+    df = pd.DataFrame(time_series, columns = ["t", "x"])
+
+    # Divide time series in training (60%), and test set (40%)
+    length = len(df)
+    training_set = "1 " + str(floor(0.6 * length))
+    test_set = str(floor(0.6 * length) + 1) + " " + str(length)
+
+    # Evaluate SMap prediction skill for localization parameter theta
+    rho_per_theta = PredictNonlinear(dataFrame = df, E = E, tau = -np.abs(lag),
+                                     lib = training_set, pred = test_set, columns = "x", embedded = False)
+
+    return(rho_per_theta)
+
 #TODO
 #S-Map
 #CCM
 #Multi-view
 #PredictInterval
 #PredictNonlinear
+#SpatialReplicates
 
 if __name__ == "__main__":
-    lorenz_trajectory = simulate_lorenz(t_max=1000)
+    lorenz_trajectory = simulate_lorenz()
     lorenz_x = lorenz_trajectory[:,0]
 
-    thomas_trajectory = simulate_thomas()
-    thomas_x = thomas_trajectory[:,0]
+    #thomas_trajectory = simulate_thomas()
+    #thomas_x = thomas_trajectory[:,0]
 
-    simplex_projection(lorenz_x)
-    simplex_projection(thomas_x)
+    #simplex_projection(lorenz_x)
+    plot_autocorrelation(lorenz_x)
+    #simplex_projection(lorenz_x)
+    #S_map(lorenz_x)
