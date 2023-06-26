991c05de1fe093a1e53f6bba0111c25f0fe17b61
Fleur Slegers <134489898+fslegers@users.noreply.github.com>
2023-06-26 13:25:46 +0200

    Implemented plotting of predicted values against observed values for optimal E and theta for Simplex and S-Map

--- src/Empirical Dynamic Modeling.py
+++ src/Empirical Dynamic Modeling.py
@@ -80,16 +80,19 @@ def my_simplex_projection(time_series, lag = 1, max_E = 10, show_plots = False):
     mae_list = []
     rmse_list = []
 
+    optimal_cor = 0
+    optimal_predictions = []
+
     # For each dimension E
     for dim in range(1, max_E + 1):
-        hankel_matrix = create_hankel_matrix(time_series, lag, dim)
-        dist_matrix = create_distance_matrix(hankel_matrix)
+        Hankel_matrix = create_hankel_matrix(time_series, lag, dim)
+        dist_matrix = create_distance_matrix(Hankel_matrix)
 
         # for all target points, get dim+1 nearest neighbors and make one-step-ahead prediction (weighted average)
         predictions = []
-        N = hankel_matrix.shape[1]
+        N = Hankel_matrix.shape[1]
 
-        for target in range(N):
+        for target in range(N-1):
             # find dim + 1 nearest neighbors
             nearest_neighbors = np.argpartition(dist_matrix[target, :], (0, dim + 2))
             nearest_neighbors = np.arange(N)[nearest_neighbors[1:dim+2]]
@@ -122,7 +125,7 @@ def my_simplex_projection(time_series, lag = 1, max_E = 10, show_plots = False):
             #TODO: in book, they have a minimum weight of 0.000001 (why?)
 
         if show_plots:
-            plt.scatter(hankel_matrix[0, :], predictions)
+            plt.scatter(Hankel_matrix[0, :], predictions)
             plt.plot(range(0,int(max(time_series))), range(0,int(max(time_series))))
             plt.title("E = " + str(dim))
             plt.xlabel("Observed values", fontsize = 12)
@@ -130,17 +133,22 @@ def my_simplex_projection(time_series, lag = 1, max_E = 10, show_plots = False):
             plt.show()
 
         # Pearson Correlation Coefficient
-        cor = pearsonr(Hankel_matrix[0, :], predictions)[0]
+        cor = pearsonr(Hankel_matrix[0, 1:], predictions)[0]
         cor_list.append(cor)
 
         # Mean Absolute Error
-        mae = mean(abs(np.subtract(Hankel_matrix[0,:],predictions)))
+        mae = mean(abs(np.subtract(Hankel_matrix[0, 1:], predictions)))
         mae_list.append(mae)
 
         # Root Mean Squared Error
-        mse = mean(np.square(np.subtract(Hankel_matrix[0,:], predictions)))
+        mse = mean(np.square(np.subtract(Hankel_matrix[0, 1:], predictions)))
         rmse = math.sqrt(mse)
         rmse_list.append(rmse)
+
+        if cor >= optimal_cor:
+            optimal_cor = cor
+            optimal_predictions = predictions
+            optimal_E = dim
 
     # Show figure of performance plots
     fig, axs = plt.subplots(3, sharex = True)
@@ -176,15 +184,30 @@ def my_simplex_projection(time_series, lag = 1, max_E = 10, show_plots = False):
 
     plt.show()
 
-
     print("Highest correlation for E = :", str(np.argmax(cor_list) + 1) + " (" + str(max(cor_list)) + ")")
     print("Lowest MAE for E = :", str(np.argmin(mae_list) + 1) + " (" + str(min(mae_list)) + ")")
     print("Lowest RMSE for E = :", str(np.argmin(rmse_list) + 1) + " (" + str(min(rmse_list)) + ")")
 
-    return np.argmax(cor_list) + 1
+    # Plot predicted values against actual values for optimal E
+    Hankel_matrix = create_hankel_matrix(time_series, lag, E = optimal_E)
+    xmin = min(min(optimal_predictions), min(Hankel_matrix[0, 1:]))
+    xmax = max(max(optimal_predictions), max(Hankel_matrix[0, 1:]))
 
-def simplex_projection_replicates(time_series, lag = -1, max_E = 10):
-    return 0
+    xmin = xmin - 0.1 * np.abs(xmin)
+    xmax = xmax + 0.1 * np.abs(xmax)
+    plt.xlim((xmin, xmax))
+    plt.ylim((xmin, xmax))
+
+    plt.plot([xmin, xmax], [xmin, xmax], color='black')
+    plt.scatter(Hankel_matrix[0, 1:], optimal_predictions, color='black')
+
+    plt.xlabel("Observed values")
+    plt.ylabel("Predicted values")
+    plt.title("Simplex results for E = " + str(np.argmax(cor_list) + 1))
+
+    plt.show()
+
+    return optimal_E
 
 def S_map(time_series, lag = -1, E = 10):
     """
@@ -217,35 +240,26 @@ def S_map(time_series, lag = -1, E = 10):
     return(rho_per_theta)
 
 def my_S_map(time_series, lag = 1, E = 2):
-    # Embed time series
-    N = len(time_series) - E*lag
-    Hankel_matrix = []
-    for i in range(E + 1):
-        if i == 0:
-            delayed_time_series = time_series[E * lag:]
-        else:
-            delayed_time_series = time_series[(E - i) * lag:-i * lag]
-        Hankel_matrix.append(delayed_time_series)
+    Hankel_matrix = create_hankel_matrix(time_series, lag, E)
+    dist_matrix = create_distance_matrix(Hankel_matrix)
 
-    # create distance matrix
-    Hankel_matrix = np.stack(Hankel_matrix, axis=0)
-    dist_matrix = np.zeros((N, N))
-    for i in range(N):
-        for j in range(i, N):
-            dist = np.linalg.norm((Hankel_matrix[:, i] - Hankel_matrix[:, j]))
-            dist_matrix[i, j] = dist
-            dist_matrix[j, i] = dist
-
+    N = Hankel_matrix.shape[1]
     cor_list = []
     mae_list = []
     rmse_list = []
+
+    optimal_cor = 0
+    optimal_theta = 0
+    optimal_predictions = []
 
     for theta in range(11):
 
         predictions = []
 
-        for target in range(N):
-            d_m = mean(np.concatenate((dist_matrix[target,:target], dist_matrix[target, target+1:])))
+        # Make a one-step-ahead prediction for all points in state space
+        # except the last observed point
+        for target in range(N-1):
+            d_m = mean(np.concatenate((dist_matrix[target, :target], dist_matrix[target, target+1:])))
 
             if d_m == 0:
                 print('Distance to all points is zero.')
@@ -253,23 +267,30 @@ def my_S_map(time_series, lag = 1, E = 2):
 
             weights = np.exp(-theta * dist_matrix[target, :] / d_m)
             weights[target] = 0
-            next_val = np.dot(weights,np.transpose(time_series[1:N+1])) / sum(weights)
+            next_val = np.dot(weights, np.transpose(time_series[1:N+1])) / sum(weights)
             predictions.append(next_val)
 
         # Pearson Correlation Coefficient
-        cor = pearsonr(Hankel_matrix[0, :], predictions)[0]
+        cor = pearsonr(Hankel_matrix[0, 1:], predictions)[0]
         cor_list.append(cor)
 
         # Mean Absolute Error
-        mae = mean(abs(np.subtract(Hankel_matrix[0, :], predictions)))
+        mae = mean(abs(np.subtract(Hankel_matrix[0, 1:], predictions)))
         mae_list.append(mae)
 
         # Root Mean Squared Error
-        mse = mean(np.square(np.subtract(Hankel_matrix[0, :], predictions)))
+        mse = mean(np.square(np.subtract(Hankel_matrix[0, 1:], predictions)))
         rmse = math.sqrt(mse)
         rmse_list.append(rmse)
 
+        # Update optimal predictions
+        if cor >= optimal_cor:
+            optimal_theta = theta
+            optimal_cor = cor
+            optimal_predictions = predictions
+
     # Show figure of performance plots
+    plt.figure(0)
     fig, axs = plt.subplots(3, sharex=True)
     fig.suptitle('Performance measures per Theta')
 
@@ -277,9 +298,9 @@ def my_S_map(time_series, lag = 1, E = 2):
     axs[1].set_ymargin(0.1)
     axs[2].set_ymargin(0.1)
 
-    axs[0].plot(range(0, 11), cor_list, color = 'black', marker = 'o')
-    axs[1].plot(range(0, 11), mae_list, color = 'black', marker = 'o')
-    axs[2].plot(range(0, 11), rmse_list, color = 'black', marker = 'o')
+    axs[0].plot(range(0, 11), cor_list, color='black', marker='o')
+    axs[1].plot(range(0, 11), mae_list, color='black', marker='o')
+    axs[2].plot(range(0, 11), rmse_list, color='black', marker='o')
 
     axs[0].set_ylabel('rho')
     axs[1].set_ylabel('MAE')
@@ -293,23 +314,40 @@ def my_S_map(time_series, lag = 1, E = 2):
     axs[1].xaxis.grid(True)
     axs[2].xaxis.grid(True)
 
-    axs[0].plot(np.argmax(cor_list), max(cor_list), color='m', marker='D', markersize = 7)
-    axs[1].plot(np.argmin(mae_list), min(mae_list), color='m', marker='D', markersize = 7)
-    axs[2].plot(np.argmin(rmse_list), min(rmse_list), color='m', marker='D', markersize = 7)
+    axs[0].plot(np.argmax(cor_list), max(cor_list), color='m', marker='D', markersize=7)
+    axs[1].plot(np.argmin(mae_list), min(mae_list), color='m', marker='D', markersize=7)
+    axs[2].plot(np.argmin(rmse_list), min(rmse_list), color='m', marker='D', markersize=7)
 
     plt.show()
 
-#TODO: put "embed time series" and "create distance matrix" into own functions
-#TODO: Simplex plot predicted values against actual values for optimal E
-#TODO: S-Map plot predicted values against actual values for optimal theta
-#TODO: Make thing parallel
+    # Plot predicted values against actual values for optimal theta
+    plt.figure(1)
+    xmin = min(min(optimal_predictions), min(Hankel_matrix[0, 1:]))
+    xmax = max(max(optimal_predictions), max(Hankel_matrix[0, 1:]))
 
+    xmin = xmin - 0.1 * np.abs(xmin)
+    xmax = xmax + 0.1 * np.abs(xmax)
+    plt.xlim((xmin, xmax))
+    plt.ylim((xmin, xmax))
+
+    plt.plot([xmin, xmax], [xmin, xmax], color='black')
+    plt.scatter(Hankel_matrix[0, 1:], optimal_predictions, color='black')
+
+    plt.xlabel("Observed values")
+    plt.ylabel("Predicted values")
+    plt.title("Scatter plot for E = " + str(E) + r" and $\theta$ = " + str(optimal_theta))
+
+    plt.show()
+
+    return(optimal_theta)
+
+#TODO: Make things parallel
 
 if __name__ == "__main__":
-    #lorenz_trajectory = simulate_lorenz(t_max = 1000, noise = 1.5)
-    #lorenz_y = lorenz_trajectory[250:,1]
+    #lorenz_trajectory = simulate_lorenz(t_max=1000, noise=0)
+    #lorenz_y = lorenz_trajectory[250:, 1]
 
-    #white_noise = simulate_additive_white_noise(delta_t = 1, t_max = 300, noise = 0.5)
+    white_noise = simulate_additive_white_noise(delta_t=2, t_max=1500, noise=2.5)
 
     #thomas_trajectory = simulate_thomas()
     #thomas_x = thomas_trajectory[:,0]
@@ -321,13 +359,14 @@ if __name__ == "__main__":
 
     #plot_embedding(lorenz_y, E = 3, lag = 181, filename = "")
 
-    time_series = [1,2,3,4,5,6,7,8,9,10]
+    time_series = white_noise
+    #time_series = lorenz_y
 
-    # plot_time_series(time_series)
+    plot_time_series(time_series)
     # plot_autocorrelation(time_series)
     # plot_partial_autocorrelation(time_series)
     # plot_recurrence(time_series[1:100], delay = 1)
     # make_lag_scatterplot(time_series, lag = 1)
 
-    my_simplex_projection(time_series, lag = 1, max_E = 3, show_plots = False)
-    my_S_map(time_series, lag = 1, E = 3)
+    optimal_E = my_simplex_projection(time_series, lag=1, max_E=10, show_plots=False)
+    my_S_map(time_series, lag=1, E=optimal_E)
